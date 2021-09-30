cfa101fe1d08f95268773f76d794c92842fbcee5
Alena Maramygina <maratustraa@gmail.com>
2021-09-30 17:37:06 +0500

    в script.js созданы переменные, команда вызова модального окна и команда вывода в консоль, в index.html изменен заголовок документа

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("Hi there, nice to meet you!");
+/*jshint esversion: 6 */
+
+const title = "Первый скрипт";
+let screens = 2;
+let screenPrice = 500;
+let rollback = true;
+let fullPrice = 1000;
+let adaptive = false;
+
+alert("Переменные созданы!");
+console.log("Это невероятно!");
+
+
