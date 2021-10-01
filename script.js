253f5d4e8a41049a1864e67a3b1f033b9eaebc66
Alena Maramygina <maratustraa@gmail.com>
2021-10-01 15:27:43 +0500

    в файле script.js выполнено задание по уроку 2

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 /*jshint esversion: 6 */
 
-const title = "Первый скрипт";
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+const title = "Верстка сайта";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 500;
+let rollback = 30;
+let fullPrice = 10000;
+let adaptive = true;
 
-alert("Переменные созданы!");
-console.log("Это невероятно!");
+// Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
 
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 
+// Вывести в консоль длину строки screens
+
+console.log(screens.length);
+
+// Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” 
+// и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей/долларов/гривен/юаней");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей/долларов/гривен/юаней");
+
+// Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
+
+console.log(screens.toLowerCase().split());
+
+// Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
+
+console.log("Процент отката посреднику за работу " + fullPrice * (rollback / 100));
