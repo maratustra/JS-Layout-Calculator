bb09f0c7f13554b6143faffb9db8ff93e5a6ead5
Alena Maramygina <maratustraa@gmail.com>
2021-10-01 11:17:21 +0500

    убрала все значения переменных, кроме title (выдает ошибку)

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 /*jshint esversion: 6 */
 
 const title = "Первый скрипт";
-let screens = 2;
-let screenPrice = 500;
-let rollback = true;
-let fullPrice = 1000;
-let adaptive = false;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
 
 alert("Переменные созданы!");
 console.log("Это невероятно!");
