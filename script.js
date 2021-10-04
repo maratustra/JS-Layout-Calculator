3a1d003c376d80768beda948a01b5533df3e7e63
Alena Maramygina <maratustraa@gmail.com>
2021-10-04 16:47:20 +0500

    выполнено задание к уроку 3

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /*jshint esversion: 6 */
+'use strict';
 
-const title = "Верстка сайта";
+let title = "Верстка сайта";
 let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 500;
 let rollback = 30;
@@ -9,24 +10,89 @@ let adaptive = true;
 
 // Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
+// console.log(typeof title);
+// console.log(typeof fullPrice);
+// console.log(typeof adaptive);
 
 // Вывести в консоль длину строки screens
 
-console.log(screens.length);
+// console.log(screens.length);
 
 // Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” 
 // и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
 
-console.log("Стоимость верстки экранов " + screenPrice + " рублей");
-console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+// console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+// console.log("Стоимость разработки сайта " + fullPrice + " рублей");
 
 // Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
 
-console.log(screens.toLowerCase().split(''));
+// console.log(screens.toLowerCase().split(''));
 
 // Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
 
-console.log("Процент отката посреднику за работу " + fullPrice * (rollback / 100));
+// console.log("Процент отката посреднику за работу " + fullPrice * (rollback / 100));
+
+// Спрашиваем у пользователя “Как называется ваш проект?” и результат сохраняем в переменную title
+
+title = prompt("Как называется ваш проект?");
+
+/* Спросить у пользователя “Какие типы экранов нужно разработать?”, 
+сохранить в переменную screens (пример: "Простые, Сложные, Интерактивные") */
+
+screens = prompt("Какие типы экранов нужно разработать?");
+
+/* Спросить у пользователя “Сколько будет стоить данная работа?” 
+и сохранить в переменную screenPrice (пример: 12000) */
+
+screenPrice = +prompt("Сколько будет стоить данная работа?", "укажите цену в рублях");
+
+/* Спросить у пользователя “Нужен ли адаптив на сайте?” и 
+сохранить данные в переменной adaptive (булево значение true/false) */
+
+let adaptationQuestion = prompt("Нужен ли адаптив на сайте?", "да/нет");
+
+if (adaptationQuestion == 'да') adaptive = true;
+else adaptive = false;
+
+/* Спросить у пользователя по 2 раза каждый вопрос и записать ответы в разные переменные 
+1. “Какой дополнительный тип услуги нужен?” (например service1, service2) 
+2. “Сколько это будет стоить?” (например servicePrice1, servicePrice2) 
+в итоге 4 вопроса и 4 разные переменных, вопросы задаются в такой последовательности 
+Название - Стоимость - Название - Стоимость */
+
+let extraService1 = prompt("Какой дополнительный тип услуги вам нужен?");
+let extraServicePrice1 = +prompt("Сколько это будет стоить?", "укажите цену в рублях");
+
+let extraService2 = prompt("Какой дополнительный тип услуги вам нужен?");
+let extraServicePrice2 = +prompt("Сколько это будет стоить?", "укажите цену в рублях");
+
+/* Вычислить итоговую стоимость работы, учитывая стоимость верстки экранов 
+и дополнительных услуг (screenPrice + servicePrice1 + servicePrice2), 
+и результат занести в переменную fullPrice */
+
+fullPrice = screenPrice + extraServicePrice1 + extraServicePrice2;
+
+/* Объявить переменную servicePercentPrice и занести в нее итоговую стоимость 
+за вычетом процента отката (fullPrice - Процент отката за работу), 
+округляя в большую сторону (методы объекта Math в помощь) и вывести ее в консоль */
+
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
+console.log(servicePercentPrice);
+
+/* Написать конструкцию условий (расчеты приведены в рублях): 
+  - Если fullPrice больше 30000, то “Даем скидку в 10%”, 
+  - Если fullPrice больше 15000 и меньше 30000, то сообщение “Даем скидку в 5%”, 
+  - Если fullPrice меньше 15000 и больше 0 то в консоль вывести сообщение “Скидка не предусмотрена”, 
+  - Если отрицательное значение то вывести “Что то пошло не так”, 
+  - Учесть варианты 0, 15000 и 30000 (к какому уровню не важно) */
+
+if (fullPrice >= 30000) {
+  console.log("Даем скидку в 10%");
+} else if (fullPrice < 30000 || fullPrice >= 15000) {
+  console.log("Даем скидку в 5%");
+} else if (fullPrice < 15000 || fullPrice > 0) {
+  console.log("Скидка не предусмотрена");
+} else {
+  console.log("Что-то пошло не так");
+}
+
