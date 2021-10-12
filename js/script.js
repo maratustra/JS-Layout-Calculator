b39121c3075bc0ec068d9e7aa83f7d28903a3744
Alena Maramygina <maratustraa@gmail.com>
2021-10-12 19:17:46 +0500

    добавлены объявления переменных

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const title = document.getElementsByTagName('h1')[0];
+
+const buttons = document.getElementsByClassName('handler_btn');
+
+const plusButton = document.querySelector('.screen-btn');
+
+const percentItems = document.querySelectorAll('.other-items.percent');
+
+const numberItems = document.querySelectorAll('.other-items.number');
+
+const inputs = document.querySelector('.rollback [type="range"]');
+
+const spans = document.querySelector('.rollback span.range-value');
+
+const totalInputs = document.getElementsByClassName('total-input');
+
+for (let eachInput = 0; eachInput < totalInputs.length; eachInput++) {
+  totalInputs[eachInput];
+}
+
+let screenBlocks = document.querySelectorAll('div.screen');
