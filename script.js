a2734579a1dee1fc4e8ddc571808666a68931557
Alena Maramygina <maratustraa@gmail.com>
2021-10-15 16:50:32 +0500

    добавлены переменные, полученные через DOM

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 /*jshint esversion: 6 */
 'use strict';
 
+const title = document.getElementsByTagName('h1')[0];
+const buttonPlus = document.querySelector('.screen-btb');
+const otherItemsPercent = document.querySelectorAll('.other-items.percent');
+const otherItemsNumber = document.querySelectorAll('.other-items.number');
+
+const inputRange = document.querySelector('.rollback input');
+const inputRangeValue = document.querySelector('.rollback .range-value');
+
+const startBtn = document.getElementsByClassName('handler_btn')[0];
+const resetBtn = document.getElementsByClassName('handler_btn')[1];
+
+const total = document.getElementsByClassName('total-input')[0];
+const totalCount = document.getElementsByClassName('total-input')[1];
+const totalCountOther = document.getElementsByClassName('total-input')[2];
+const fullTotalCount = document.getElementsByClassName('total-input')[3];
+const totalCountRollback = document.getElementsByClassName('total-input')[4];
+
+let screens = document.querySelectorAll('.screen');
+
 const appData = {
   title: '',
   screens: [],
